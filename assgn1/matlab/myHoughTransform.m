346ec21781b96b95e41430d324c5713bfeda3251
mcreng <tsehonam@gmail.com>
2019-09-15 15:30:37 +0800

    assign1: finished Q2.3

--- assgn1/matlab/myHoughTransform.m
+++ assgn1/matlab/myHoughTransform.m
@@ -1,5 +1,25 @@
 function [H, rhoScale, thetaScale] = myHoughTransform(Im, threshold, rhoRes, thetaRes)
 %Your implementation here
+   
+    % threshold the image
+    Im(Im < threshold) = 0;
+    
+    % create theta scale
+    rhoScale = linspace(0, sum(size(Im), 'all'), sum(size(Im), 'all') / rhoRes);
+    thetaScale = linspace(0, 2*pi, 2*pi / thetaRes);
+    
+    % generate rho-theta pairs
+    [Y, X] = find(Im);
+    for theta = thetaScale
+        rho = X * cos(theta) + Y * sin(theta);
+        tH = histcounts2(rho, ones(size(rho)) * theta, rhoScale, thetaScale);
+        
+        if exist('H', 'var'); H = H + tH; else H = tH; end
+        
+    end
+    
+    H = H / max(H(:));
+    
 end
         
         
