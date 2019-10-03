8e370c25a9ef1f5eeb9cba45552851f80781fceb
mcreng <tsehonam@gmail.com>
2019-10-03 23:46:24 +0800

    assign2: fixed computeH

--- assgn2/matlab/computeH.m
+++ assgn2/matlab/computeH.m
@@ -5,13 +5,8 @@ A = [];
 for i = 1:N
     A(2*i-1:2*i, :) = computeAi(x1(i, :), x2(i, :));
 end
-% A = zeros(2*N, 9);
-% A(2*(1:N)-1:2*(1:N), :) = computeAi(x1(1:N, :), x2(1:N, :));
-[~, S, V] = svd(A, 'econ');
-disp('A'); disp(A); disp(A(2*(1:N)));
-s = diag(S);
-disp('S'); disp(s); disp(find(s)); disp(size(S)); disp(s(6:end));
-disp('V'); disp(V); disp(size(V));
+
+[~, ~, V] = svd(A);
 H2to1 = reshape(V(:, end), [3 3])';
 end
 
--- assgn2/matlab/testComputeH.m
+++ assgn2/matlab/testComputeH.m
@@ -1,38 +1,17 @@
 function testComputeH()
 
 x1 = [333 484; 333 334; 709 334; 709 484];
-% x1 = [0 0; 100 0; 100 200; 0 200];
-% x2 = [332 343; 430 229; 712 477; 614 590];
+
 theta = 45 * pi/180;
 rot = [cos(theta) -sin(theta); sin(theta) cos(theta)];
 rothomo = [cos(theta) -sin(theta) 0; sin(theta) cos(theta) 0; 0 0 1];
 x2 = (rot * x1')';
-disp('x2');
-disp(x2)
-
-x1homo = horzcat(x1, ones(size(x1, 1), 1));
-x2homo = horzcat(x2, ones(size(x1, 1), 1));
 
 H2to1 = computeH(x1, x2);
-disp('H');
-disp(H2to1);
-disp('h * x2homo');
-x1_ = H2to1 * x2homo';
-disp(x1_);
-disp('x1');
-disp(x1homo');
-disp('compare: test that they should share the same scalar scale');
-scales = x1_ ./ x1homo';
-disp(scales);
-disp('max');
-max_ = max(max(scales));
-disp(max_);
-disp('min');
-min_ = min(min(scales));
-disp(min_);
-disp('diff');
-disp(max_ - min_);
-disp('h_ans/h2to1');
-disp(inv(rothomo) ./ H2to1);
+
+disp('H'); disp(H2to1);
+
+% Sanity check: non-zero elements should be the same
+disp('alpha'); disp(inv(rothomo) ./ H2to1);
 
 end
