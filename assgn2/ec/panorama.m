1b57e34bef0ebd4308c699163e3a5413cfda1e5b
Bryan Chun <chun.hiu.sang@gmail.com>
2019-10-09 01:15:37 +0800

    assign2: started Q4.2x

--- assgn2/ec/panorama.m
+++ assgn2/ec/panorama.m
@@ -1,1 +1,8 @@
 % Q4.3x
+clear all;
+
+pano_left = imread('../data/pano_left.jpg');
+pano_right = imread('../data/pano_right.jpg');
+% TODO: Change to own images
+
+locPairs = cpselect(pano_left, pano_right);
