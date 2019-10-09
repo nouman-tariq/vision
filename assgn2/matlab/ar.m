79e0077fb5964bf38db9f49bea9988439f7a21e5
mcreng <tsehonam@gmail.com>
2019-10-09 23:09:29 +0800

    assign2: draft on ar.m

--- assgn2/matlab/ar.m
+++ assgn2/matlab/ar.m
@@ -1,1 +1,28 @@
 % Q3.3.1
+clear all;
+close all;
+
+ar_src = loadVid('../data/ar_source.mov');
+in = loadVid('../data/book.mov');
+cv_img = imread('../data/cv_cover.jpg');
+
+out = VideoWriter('output.avi');
+open(out)
+
+for i = 1:length(ar_src)
+    disp(i);
+    %% Extract features and match
+    [locs1, locs2] = matchPics(cv_img, in(i).cdata);
+
+    %% Compute homography using RANSAC
+    [bestH2to1, ~] = computeH_ransac(locs1, locs2)
+    
+    %% Scale harry potter image to template size
+    % Why is this is important?
+    scaled_ar_img = imresize(ar_src(i).cdata, [size(cv_img,1) size(cv_img,2)]);
+
+    %% Write composite image
+    % imshow(compositeH(inv(bestH2to1), scaled_ar_img, in(1).cdata));
+    writeVideo(out, compositeH(inv(bestH2to1), scaled_ar_img, in(i).cdata));
+end
+close(out)
