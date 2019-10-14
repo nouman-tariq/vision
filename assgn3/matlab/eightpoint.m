6857f9138e96955a18c353c856f93e688041ca54
Bryan Chun <chun.hiu.sang@gmail.com>
2019-10-15 00:08:26 +0800

    assign3: Q3.1.1 started

--- assgn3/matlab/eightpoint.m
+++ assgn3/matlab/eightpoint.m
@@ -7,3 +7,38 @@ function F = eightpoint(pts1, pts2, M)
 % Q2.1 - Todo:
 %     Implement the eightpoint algorithm
 %     Generate a matrix F from correspondence '../data/some_corresp.mat'
+
+
+%% Normalize points
+pts1 = pts1 ./ M;
+pts2 = pts2 ./ M;
+
+%% Construct N-by-9 'A'
+N = size(pts1, 1);
+A = zeros(N, 9);
+for i = 1:N
+    A(i, :) = computeAi(pts1(i, :), pts2(i, :));
+end
+
+%% Find SVD of 'A' to find 'F'
+[~, ~, V_A] = svd(A);
+F = reshape(V_A(end, :), [3 3]);
+
+%% Enforce rank 2 constraint on 'F'
+[U, S, V] = svd(F);
+S(end, end) = 0;
+F = U*S*V';
+
+%% Refine solution with local minimization
+F = refineF(F, pts1, pts2);
+
+%% Un-normalize 'F'
+% scale back up .* M
+F = F .* M;
+end
+
+function Ai = computeAi(pt1, pt2)
+x = pt1(1); y = pt1(2);
+x_ = pt2(1); y_ = pt2(2);
+Ai = [x*x_ x*y_ x y*x_ y*y_ y x_ y_ 1];
+end
--- assgn3/matlab/testTempleCoords.m
+++ assgn3/matlab/testTempleCoords.m
@@ -3,5 +3,15 @@
 % Write your code here
 %
 
+%% 1. Load images and correspondences
+im1 = imread('../data/im1.png');
+im2 = imread('../data/im2.png');
+corresp = load('../data/someCorresp.mat');
+
+%% 2. Run eightpoint to compute fundamental matrix
+F = eightpoint(corresp.pts1, corresp.pts2, corresp.M);
+displayEpipolarF(im1, im2, F);
+
+%% 8. Save computed rotation matrix and translation
 % save extrinsic parameters for dense reconstruction
 save('../data/extrinsics.mat', 'R1', 't1', 'R2', 't2');
