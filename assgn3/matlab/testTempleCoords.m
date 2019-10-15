dc1256f54eccf43d1b5ab4271164abe5bae6bbbe
mcreng <tsehonam@gmail.com>
2019-10-15 22:58:12 +0800

    assign3: finished Q3.1.3

--- assgn3/matlab/testTempleCoords.m
+++ assgn3/matlab/testTempleCoords.m
@@ -13,6 +13,11 @@ F = eightpoint(corresp.pts1, corresp.pts2, corresp.M);
 disp('F'); disp(F);
 displayEpipolarF(im1, im2, F);
 
+%% 4. Load intrisincs.mat and compute essential matrix
+intrinsics = load('../data/intrinsics.mat');
+E = essentialMatrix(F, intrinsics.K1, intrinsics.K2);
+disp('E'); disp(E);
+
 %% 8. Save computed rotation matrix and translation
 % save extrinsic parameters for dense reconstruction
 save('../data/extrinsics.mat', 'R1', 't1', 'R2', 't2');
