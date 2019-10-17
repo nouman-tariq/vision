9a7d9e67fcc3b385f412afbb469203eb8f5a115d
Bryan Chun <chun.hiu.sang@gmail.com>
2019-10-18 00:59:49 +0800

    assign3: drafted on Q3.1.{4,5}

--- assgn3/matlab/testTempleCoords.m
+++ assgn3/matlab/testTempleCoords.m
@@ -19,13 +19,65 @@ F = eightpoint(corresp.pts1, corresp.pts2, corresp.M);
 %% 3. Run epipolarCorrespondences on points in img1 to get points in img2
 coords = load('../data/templeCoords.mat');
 coords.pts2 = epipolarCorrespondence(im1, im2, F, coords.pts1);
-epipolarMatchGUI(im1, im2, F);
+% epipolarMatchGUI(im1, im2, F);
 
 %% 4. Load intrisincs.mat and compute essential matrix
 intrinsics = load('../data/intrinsics.mat');
 E = essentialMatrix(F, intrinsics.K1, intrinsics.K2);
 disp('E'); disp(E);
 
+%% 5. Compute camera projection matrices P1, and P2 by camera2
+extrinsic1 = [eye(3), zeros(3, 1)];
+P1 = intrinsics.K1 * extrinsic1;
+% 4 candidates
+extrinsic2s = camera2(E);
+P2s = zeros(size(extrinsic2s));
+for i = 1:4
+    P2s(:, :, i) = intrinsics.K2 * extrinsic2s(:, :, i);
+end
+
+%% 6. Run triangulate on the 4 candidates
+[P2, pts3d] = findExtrinsic2(P1, P2s, coords.pts1, coords.pts2);
+
+% Re-projection error using someCorresp
+repj_error = reprojectTriangulated(P1, P2s, coords.pts1, coords.pts2);
+disp('repj_error'); disp(repj_error);
+
+%% 7. Plot 3D point correspondences using plot3
+plot3(pts3d(:, 1), pts3d(:, 3), pts3d(:, 2), '.');
+
 %% 8. Save computed rotation matrix and translation
+R1 = P1(:, 1:3);
+t1 = P1(:, 4);
+R2 = P2(:, 1:3);
+t2 = P2(:, 4);
 % save extrinsic parameters for dense reconstruction
 save('../data/extrinsics.mat', 'R1', 't1', 'R2', 't2');
+
+%% Find the correct P2 and 3D points from 4 candidate extrinsics
+function [P2, pts3d] = findExtrinsic2(P1, P2s, pts1, pts2)
+pts3ds = zeros([size(pts1, 1), 3, 4]);
+numPointsWithPosDepth = zeros(4, 1);
+for i = 1:4
+    pts3ds(:, :, i) = triangulate(P1, pts1, P2s(:, :, i), pts2);
+    % which most of the 3D points are in front of both cameras (positive
+    % depth)? How
+    numPointsWithPosDepth(i) = sum(pts3ds(:, 3, i) > 0);
+end
+[~, idxExtrinsic2] = max(numPointsWithPosDepth);
+P2 = P2s(:, :, idxExtrinsic2);
+pts3d = pts3ds(:, :, idxExtrinsic2);
+end
+
+%% Compute reprojection error of triangulate
+function [err] = reprojectTriangulated(P1, P2s, pts1, pts2)
+[P2, pts3d] = findExtrinsic2(P1, P2s, pts1, pts2);
+
+N = size(pts3d, 1);
+pts1_proj = (P1 * [pts3d ones(N,1)]')';
+pts1_proj = pts1_proj(:,1:2) ./ pts1_proj(:,3);
+pts2_proj = (P2 * [pts3d ones(N,1)]')';
+pts2_proj = pts2_proj(:,1:2) ./ pts2_proj(:,3);
+
+err = (sum(vecnorm(pts1_proj - pts1, 2, 2)) + sum(vecnorm(pts2_proj - pts2, 2, 2))) ./ 2*N;
+end
--- assgn3/matlab/triangulate.m
+++ assgn3/matlab/triangulate.m
@@ -9,3 +9,18 @@ function pts3d = triangulate(P1, pts1, P2, pts2 )
 %   Returns:
 %       Pts3d:  coordinates of 3D points with shape N x 3
 %
+N = size(pts1,1);
+pts3d = zeros(N,3);
+for i = 1:N
+    x1 = pts1(i,1);
+    y1 = pts1(i,2);
+    x2 = pts2(i,1);
+    y2 = pts2(i,2);
+    A = [y1.*P1(3,:) - P1(2,:);...
+         P1(1,:) - x1.*P1(3,:);...
+         y2.*P2(3,:) - P2(2,:);...
+         P2(1,:) - x2.*P2(3,:)];
+    [~, ~, V] = svd(A);
+    v = V(:, end);  
+    pts3d(i,:) = v(1:3) ./ v(4);
+end
