1063471ec22dd213c053bed62da6d148cf92a761
mcreng <tsehonam@gmail.com>
2019-10-23 18:25:37 +0800

    assign3: reduced proj error to 0.5

--- assgn3/matlab/eightpoint.m
+++ assgn3/matlab/eightpoint.m
@@ -3,7 +3,7 @@ function F = eightpoint(pts1, pts2, M)
 %   pts1 - Nx2 matrix of (x,y) coordinates
 %   pts2 - Nx2 matrix of (x,y) coordinates
 %   M    - max (imwidth, imheight)
-
+% 
 % Q2.1 - Todo:
 %     Implement the eightpoint algorithm
 %     Generate a matrix F from correspondence '../data/some_corresp.mat'
@@ -23,12 +23,12 @@ x1 = pts1n(:, 1);
 y1 = pts1n(:, 2);
 x2 = pts2n(:, 1);
 y2 = pts2n(:, 2);
-A = [x2.*x1, x2.*y1, x2, y2.*x1, y2.*y1, y2, x1, y1, ones(N, 1)];
+A = [x1.*x2, x1.*y2, x1, y1.*x2, y1.*y2, y1, x2, y2, ones(N, 1)];
 
 %% Find SVD of 'A' to find 'F'
 [~, ~, V_A] = svd(A);
 % 'V' is returned transposed
-F = reshape(V_A(:, 9), [3 3]);
+F = reshape(V_A(:, end), [3 3]);
 
 %% Enforce rank 2 constraint on 'F'
 [U, S, V] = svd(F);
@@ -36,7 +36,7 @@ S(end, end) = 0;
 F = U*S*V';
 
 %% Refine solution with local minimization
-F = refineF(F, pts1n(:, 1:2), pts2n(:, 1:2));
+F = refineF(F, pts1n, pts2n);
 
 %% Un-normalize 'F'
 % Scale up by 'M'
