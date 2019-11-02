61425c65bf776b4ceedf93131c89dc624c6dac6a
mcreng <tsehonam@gmail.com>
2019-11-02 14:51:55 +0800

    assign5: fixed shape of trainFeatures

--- assgn5/matlab/buildRecognitionSystem.m
+++ assgn5/matlab/buildRecognitionSystem.m
@@ -10,7 +10,7 @@ for i = 1:length(train_imagenames)
     matname = strcat(datadir, train_imagenames{i});
     matname = strrep(matname,'.jpg','.mat');
     load(matname, 'wordMap');
-    trainFeatures = [trainFeatures; getImageFeatures(wordMap, size(dictionary, 1))];
+    trainFeatures = [trainFeatures; getImageFeatures(wordMap, size(dictionary, 1))'];
 end
 save('visionRandom.mat', 'dictionary', 'filterBank', 'trainFeatures', 'trainLabels');
 
@@ -21,6 +21,6 @@ for i = 1:length(train_imagenames)
     matname = strcat(datadir, train_imagenames{i});
     matname = strrep(matname,'.jpg','.mat');
     load(matname, 'wordMap');
-    trainFeatures = [trainFeatures; getImageFeatures(wordMap, size(dictionary, 1))];
+    trainFeatures = [trainFeatures; getImageFeatures(wordMap, size(dictionary, 1))'];
 end
 save('visionHarris.mat', 'dictionary', 'filterBank', 'trainFeatures', 'trainLabels');
